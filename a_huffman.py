16435b9c6bc111d3be7c30df82be5c4fdf4cda67
YD620 <noobaf850@gmail.com>
2023-06-09 22:33:47 -0700

    To Be graded

--- a_huffman.py
+++ a_huffman.py
@@ -4,6 +4,7 @@
 
 # the input, what we want to encode
 message: str = 'Hello there'
+message = message.upper()
 
 # the output, should be all 0's and 1s
 result: str = str()
@@ -23,29 +24,41 @@ coding: dict = dict()   # key  -> a letter
 # STEP 0 - TODO
 ## defining our data structures
 class Node: # NOT given to students
-    # TODO
-    
-    def __init__(self):
+    weight: int
+    letters: str
+    left: any
+    right: any
+
+    def __init__(self, weight = None, letters = None, left = None, right = None):
+        self.weight = weight
+        self.letters= letters
+        self.left = left
+        self.right = right
         return
 
 ## defining operations
 ### recursively traverses the huffman tree to record the codes
 def retrieve_codes(v: Node, path: str=''):
     global coding
-    if v.letter != None: # if 'TODO': # TODO
-        coding[v.letter] = None # TODO
+    if v.letters != None: # if 'TODO': # TODO
+        coding[v.letters] = path # TODO
     else:
-        retrieve_codes(None, None) # TODO
-        retrieve_codes(None, None) # TODO
+        retrieve_codes(v.left, path + '0') # TODO
+        retrieve_codes(v.right, path + '1') # TODO
 
 # STEP 1
 ## counting the frequencies - TODO
+for i in message:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
 
 
 # STEP 2
 ## initialize the nodes - TODO
-nodes = list()
-nodes.append(Node(0, 'a'))
+for letters, count in freq.items():
+    nodes.append(Node(count, letters))
 
 # STEP 3 - TODO
 ## combine each nodes until there's only one item in the nodes list
@@ -60,7 +73,7 @@ while len(nodes) > 1:
     min_b: Node = nodes.pop()
 
     ## combine the two
-    combined: Node = None # TODO
+    combined = Node(min_a.weight + min_b.weight, None, min_a, min_b) # TODO
 
     ## put the combined nodes back in the list of nodes
     nodes.append(combined)
@@ -69,7 +82,8 @@ while len(nodes) > 1:
 ## reconstruct the codes
 huff_root = nodes[0]
 retrieve_codes(huff_root)
-result: str = str() # TODO (hint coding[letter] -> code)
+for i in message:
+    result += str(coding[i]) # TODO (hint coding[letter] -> code)
 
 # STEP 5
 ## analyize compression performance
--- b_huffman-analysis.py
+++ b_huffman-analysis.py
@@ -5,9 +5,9 @@
 import matplotlib.pyplot as plt
 
 # DATA - lyrics
-POKEMON_LYRICS = 'I wanna be the very best. Like no one ever was. To catch them is my real test. To train them is my cause. I will travel across the land. Searching far and wide. Each Pokemon to understand. The power that\'s inside. (Pokemon, gotta catch \'em all.) Its you and me. I know it\'s my destiny. (Pokemon.) Oh, you\'re my best friend. In a world we must defend. (Pokemon, gotta catch \'em all.) A heart so true. Our courage will pull us through. You teach me and I\'ll teach you. Pokemon. (gotta catch \'em all.) Gotta catch \'em all. Yeah. Every challenge along the way. With courage I will face. I will battle every day. To claim my rightful place. Come with me, the time is right. There\'s no better team. Arm in arm we\'ll win the fight. It\'s always been our dream. (Pokemon, gotta catch \'em all.) Its you and me. I know it\'s my destiny (Pokemon.) Oh, you\'re my best friend. In a world we must defend. (Pokemon, gotta catch \'em all.) A heart so true. Our courage will pull us through. You teach me and I\'ll teach you. Pokemon (gotta catch \'em all.) Gotta catch \'em all. Gotta catch \'em all. Gotta catch \'em all. Gotta catch \'em all. Yeah! (Pokemon, gotta catch \'em all). Its you and me. I know it\'s my destiny. (Pokemon) Oh, you\'re my best friend. In a world we must defend. (Pokemon, gotta catch \'em all.) A heart so true. Our courage will pull us through. You teach me and I\'ll teach you Pokemon. (gotta catch \'em all). Gotta catch \'em all. (Pokemon)'
-JIGGLE_JIGGLE = 'You have to have something that sticks. You have to have something that\'s monumental. When you walk out on stage, that\'s been monumental. (Jiggle, jiggle) Can you remember any of the rap that you did? My money don\'t jiggle, jiggle, it folds. I like to see you wiggle, wiggle, for sure. It makes me want to dribble, dribble, you know. Riding in my Fiat, you really have to see it. Six feet two in a compact, no slack. But luckily the seats go back. I got a knack to relax in my mind. Sipping some red, red wine. I sip booze from chalices, holding my palaces. Crib is so crampy suckers suffer from paralysis. Rhymes, I write them in the castle. You try to diss me and pretty soon your ass. Will squat in a cell \'cause I can tell you it\'s illegal. Treason, that\'s the reason I\'m regal. You do the time for the crime of lèse-majesté. And **** the police \'cause they can\'t arrest me. (They can\'t arrest me, they can\'t arrest me). (I like to see you wiggle, it makes me dribble, fancy a fiddle?). My money don\'t jiggle, jiggle, it folds. I like to see you wiggle, wiggle, for sure. It makes me want to dribble, dribble, you know. Riding in my Fiat, you really have to see it. Six feet two in a compact, no slack. But luckily the seats go back. I got a knack to relax in my mind. Sipping some red, red wine. (I like to see you wiggle, it makes me dribble, fancy a fiddle?). (I like to see you wiggle, it makes me dribble, fancy a fiddle?). (I like to see you wiggle, it makes me dribble, fancy a fiddle?). (I like to see you wiggle, it makes me dribble, fancy a fiddle?)'
-ALPHABET = 'Now it\'s time for our wrap up. Let\'s give it everything we\'ve got. Ready, begin. Artificial amateurs aren\'t at all amazing. Analytically, I assault, animate things. Broken barriers bounded by the bomb beat. Buildings are broken, basically I\'m bombarding. Casually create catastrophes, casualties. Canceling cats got their canopies collapsing. Detonate a dime of dank daily doin\' dough. Demonstrations, Don Dada on the down low. Eatin\' other editors with each and every energetic. Epileptic episode, elevated etiquette. Furious fat fabulous fantastic. Flurries of funk felt feeding the fanatics. Imitators idolize, I intimidate. In a instant, I\'ll rise in a irate state. Juiced on my jams like jheri curls, jockin\' joints. Justly, it\'s just me, writin\' my journals. Kindly I\'m kindling all kinds of ink on. Karate kick type Brits in my kingdom. Let me live a long life, lyrically lessons is. Learned lame louses just lose to my livery. My mind makes marvelous moves, masses. Marvel and move, many mock what I\'ve mastered.  Niggas nap knowin\' I\'m nice naturally. Knack, never lack, make noise nationally.  Operation, opposition, off, not optional. Out of sight, out of mind, wide beaming opticals. Perfected poem, powerful punchlines. Pummeling petty powder puffs in my prime. Quite quaint quotes keep quiet it\'s Quannum Quarrelers ain\'t got a quarter of what we got, uh. Really raw raps, risin\' up rapidly. Riding the rushing radioactivity. Super scientifical sound search sought. Silencing super fire saps that are soft. Tales ten times talented, they\'re too tough. Take that, challengers, get a tune up. Universal, unique untouched. Unadulterated, the raw uncut. Verb vice Lord victorious valid. Violate vibes that are vain make \'em vanished. Why I\'m all well, would a wise wordsmith. Just weaving up words weeded up, on my work shift. Xerox, my X-ray-diation holes extra large. X-height letters and xylophone tones.'
+DONT_SAY = 'Yesterday, We were only kids could never fade away. Running thru the wind and pouring rain.Memories at the door.Burning bright from long before.We don/t talk anymore.What are we fighting for.I went my way. You went sideways. We were always running. Out of time. Don/t say we didn/t try. Don/t say we could ever be the same together. We/re not what we used to be. Don/t say you wanted me. Don/t say you wanted me. Don/t say you wanted me. Seasons change. I kept getting colder. Breathing shades of grey. You were climbing higher you/d say. Memories at the door. Burning bright from long before. We don/t talk anymore. What are we fighting for. I went my way. You went sideways. We were always running. Out of time. Don/t say we didn/t try. Don/t say we could ever be the same together. We/re not what we used to be. Don/t say you wanted me. Don/t say you wanted me. Don/t say you wanted me. Don/t say you wanted me'
+HEAT_WAVE = 'Last night, all I think about is you. Don/t stop, baby, you can walk through. Don/t wanna, but I think about you. You know that I/m never gonna lose. Road shimmer, wiggling the vision. Heat, heat waves, I/m swimming in a mirror. Road shimmer, wiggling the vision. Heat, heat waves, I/m swimmin/ in a. Sometimes all I think about is you. Late nights in the middle of June. Heat waves been faking me out. Can/t make you happier now. Sometimes all I think about is you. Late nights in the middle of June. Heat waves been faking me out. Can/t make you happier now. Usually, I put something on TV. So we never think about you and me. But today, I see our reflections clearly in Hollywood. Laying on the screen. You just need a better life than this. You need something I can never give. Fake water all across the road. It/s gone now, the night has come, but. Sometimes all I think about is you. Late nights in the middle of June. Heat waves been faking me out. Can/t make you happier now. You can/t fight it, you can/t breathe. You say something so loving, but. Now I gotta let you go. You/ll be better off with someone new I don/t wanna be alone. You know it hurts me too. You look so broken when you cry. One more and then I say goodbye. Sometimes all I think about is you. Late nights in the middle of June. Heat waves been faking me out. Can/t make you happier now. Sometimes all I think about is you. Late nights in the middle of June. Heat waves been faking me out. Can/t make you happier now. I just wonder what you/re dreaming of. When you sleep and smile so comfortable. I just wish that I could give you that. That look that/s perfectly un-sad. Sometimes all I think about is you. Late nights in the middle of June. Heat waves been faking me out. Heat waves been faking me out. Sometimes all I think about is you. Late nights in the middle of June. Heat waves been faking me out. Can/t make you happier now. Sometimes all I think about is you. Late nights in the middle of June. Heat waves been faking me out. Can/t make you happier now. Road shimmer wiggling the vision. Heat, heat waves, I/m swimming in a mirror. Road shimmer wiggling the vision. Heat, heat waves, I/m swimming in a mirror'
+MILLION_DAYS = 'I don/t do this often. You caught me off guard. All of my friends said you moved out west. You tell me stories underneath sun sets. Could it be more than just one night? Kiss in the cab ride. Head turning left right. Get a ticket for two. To wherever you choose. Drinking some red out of the blue. You say my name with your accent. Makes me remember. How much I missed ya. Hope you/re planning to stay. For a million days. You/ll be my summer in December. Hey boy. Trust my intuitions. Always. I/m not superstitious. I got lucky. I stole your heart. And it was mine for the taking. I/m bad at pretending, you know I/ll be true. Sunday mornings, midnight lights. Turn into songs that you watch me write. Stole your sweater on that night. You took a picture. I keep by my bedside. So you/re mine when you/re gone. Baby please don/t be long. I got my ticket to the westside. You say my name with your accent. Makes me remember. How much I missed ya. Hope you/re planning to stay. For a million days. You/ll be my summer in December. Hey boy. Trust my intuitions. Always. I/m not superstitious. I got lucky. Stole your heart. And it was mine for the taking. It was mine for the taking. It was mine for a million days. Hmm, in a million ways. Stole your heart, and I got lucky. Stole your heart and it was mine in a million ways. Hmm, for a million days'
 
 # DATA - mantras
 GREEN_LATTERN = 'In brightest day, in blackest night, No evil shall escape my sight. Let those who worship evil\'s might, Beware my power... Green Lantern\'s light!'
@@ -32,22 +32,68 @@ def huffman(message:str) -> float:
     coding: dict = dict()   # key  -> a letter
                             # item -> a binary encoding
 
+# STEP 0 - TODO
+## defining our data structures
+    class Node: # NOT given to students
+        weight: int
+        letters: str
+        left: any
+        right: any
 
-    # STEP 0
-    ## defining our data structures
-    ## defining operations
+        def __init__(self, weight = None, letters = None, left = None, right = None):
+            self.weight = weight
+            self.letters= letters
+            self.left = left
+            self.right = right
 
-    # STEP 1
-    ## counting the frequencies
+## defining operations
+### recursively traverses the huffman tree to record the codes
+    def retrieve_codes(v: Node, path: str=''):
+        if v.letters != None: # if 'TODO': # TODO
+            coding[v.letters] = path # TODO
+        else:
+            retrieve_codes(v.left, path + '0') # TODO
+            retrieve_codes(v.right, path + '1') # TODO
 
-    # STEP 2
-    ## initialize the nodes
+# STEP 1
+## counting the frequencies - TODO
+    for i in message:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
 
-    # STEP 3
-    ## combine each nodes until there's only one item in the nodes list
 
-    # STEP 4
-    ## reconstruct the codes
+# STEP 2
+## initialize the nodes - TODO
+    for letters, count in freq.items():
+        nodes.append(Node(count, letters))
+
+# STEP 3 - TODO
+## combine each nodes until there's only one item in the nodes list
+    while len(nodes) > 1:
+    ## sort based on weight
+        nodes.sort(key=lambda x: x.weight, reverse=True)
+
+    ## get the first min
+        min_a: Node = nodes.pop()
+
+    ## get the second min
+        min_b: Node = nodes.pop()
+
+    ## combine the two
+        combined = Node(min_a.weight + min_b.weight, None, min_a, min_b) # TODO
+
+    ## put the combined nodes back in the list of nodes
+        nodes.append(combined)
+
+# STEP 4
+## reconstruct the codes
+    huff_root = nodes[0]
+    retrieve_codes(huff_root)
+    for i in message:
+        result += str(coding[i]) # TODO (hint coding[letter] -> code)
+
 
     # STEP 5
     ## analyize compression performance
@@ -59,52 +105,85 @@ def huffman(message:str) -> float:
 
 # LYRICS
 plt.subplot(2, 1, 1)
-plt.suptitle('Lab 7 - Stapleton Analyzing Huffman')
+plt.suptitle('Lab 7 - Ye Analyzing Huffman')
 
 MAX_N: int = int(128 * 3 / 2)
 
 # PLOT 1
 ## POKEMON
+data: str = DONT_SAY
 ratios: list = list()
-for i in range(1, MAX_N):
-    sub_message = POKEMON_LYRICS[0:i]
-    _, _, ratio = huffman(sub_message)
+for i in range(1, len(data)):
+    x = data[0: i]
+    compressed, coding, ratio = huffman(x)
     ratios.append(ratio)
+min_ratio = min(ratios)
+min_idx = ratios.index(min_ratio)
+
+plt.plot(ratios[:MAX_N], label=f'DONT_SAY (n={len(coding)})', color ='red', linestyle='dashdot')
 
 ## JIGGLE JIGGLE
+data: str = HEAT_WAVE
 ratios: list = list()
-for i in range(1, MAX_N):
-    sub_message = JIGGLE_JIGGLE[0:i]
-    _, _, ratio = huffman(sub_message)
+for i in range(1, len(data)):
+    x = data[0: i]
+    compressed, coding, ratio = huffman(x)
     ratios.append(ratio)
+min_ratio = min(ratios)
+min_idx = ratios.index(min_ratio)
+
+plt.plot(ratios[:MAX_N], label=f'HEAT_WAVE (n={len(coding)})', color ='blue', linestyle='dashdot')
 
 ## ALPHABET
+data: str = MILLION_DAYS
 ratios: list = list()
-for i in range(1, MAX_N):
-    sub_message = ALPHABET[0:i]
-    _, _, ratio = huffman(sub_message)
+for i in range(1, len(data)):
+    x = data[0: i]
+    compressed, coding, ratio = huffman(x)
     ratios.append(ratio)
+min_ratio = min(ratios)
+min_idx = ratios.index(min_ratio)
+
+plt.plot(ratios[:MAX_N], label=f'MILLION_DAYS (n={len(coding)})', color ='green', linestyle='dashdot')
 
 # PLOT 2
 plt.subplot(2, 1, 2)
 
 ## SITH CODE
+data: str = SITH_CODE
 ratios: list = list()
-for i in range(1, MAX_N):
-    sub_message = SITH_CODE[0:i]
-    _, _, ratio = huffman(sub_message)
+for i in range(1, len(data)):
+    x = data[0: i]
+    compressed, coding, ratio = huffman(x)
     ratios.append(ratio)
+min_ratio = min(ratios)
+min_idx = ratios.index(min_ratio)
+
+plt.plot(ratios[:MAX_N], label=f'SITH_CODE (n={len(coding)})', color ='red', linestyle='dashdot')
 
 ## GREEN LATERN'S OATH
+data: str = GREEN_LATTERN
 ratios: list = list()
-for i in range(1, MAX_N):
-    sub_message = GREEN_LATTERN[0:i]
-    _, _, ratio = huffman(sub_message)
+for i in range(1, len(data)):
+    x = data[0: i]
+    compressed, coding, ratio = huffman(x)
     ratios.append(ratio)
+min_ratio = min(ratios)
+min_idx = ratios.index(min_ratio)
+
+plt.plot(ratios[:MAX_N], label=f'GREEN_LATTERN (n={len(coding)})', color ='blue', linestyle='dashdot')
 
 ## JEDI CODE
+data: str = JEDI_CODE
 ratios: list = list()
-for i in range(1, MAX_N):
-    sub_message = JEDI_CODE[0:i]
-    _, _, ratio = huffman(sub_message)
+for i in range(1, len(data)):
+    x = data[0: i]
+    compressed, coding, ratio = huffman(x)
     ratios.append(ratio)
+min_ratio = min(ratios)
+min_idx = ratios.index(min_ratio)
+
+plt.plot(ratios[:MAX_N], label=f'JEDI_CODE (n={len(coding)})', color ='green', linestyle='dashdot')
+
+plt.savefig('./figs/Ye_lab7_analysis.png')
+plt.show()
